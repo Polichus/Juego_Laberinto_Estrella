6464466c17e5d6fa3dd07b990c962c7b78803367
Adri <adriacruz_@ceroca.cat>
2025-04-07 17:38:01 +0200

    instrucciones acabadas

--- Assets/Scenes/Scrips/Instrucciones.cs
+++ Assets/Scenes/Scrips/Instrucciones.cs
@@ -9,6 +9,11 @@ public class Instrucciones : MonoBehaviour
     {
         SceneManager.LoadScene("Instrucciones");
     }
+
+    public void AnarAEscenaPrincipal()
+    {
+        SceneManager.LoadScene("Pantalla Principal");
+    }
     // Start is called before the first frame update
     void Start()
     {
